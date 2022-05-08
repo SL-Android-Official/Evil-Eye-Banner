6b8511dca462ca8023972e305b9905f892a96e69
Razor Kenway <razorkenway.98@gmail.com>
2022-05-08 12:51:16 +0530

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,52 @@
+#color
+r='\e[1;31m'
+g='\e[1;32m'
+y='\e[1;33m'
+b='\e[1;34m'
+p='\e[1;35m'
+lb='\e[1;36m'
+
+
+
+
+echo
+echo -e $lb "███████╗    ██████╗  █████╗ ███╗  ██╗███╗  ██╗███████╗██████╗" 
+echo -e $lb "██╔════╝    ██╔══██╗██╔══██╗████╗ ██║████╗ ██║██╔════╝██╔══██╗"
+echo -e $lb "█████╗  ███╗██████╦╝███████║██╔██╗██║██╔██╗██║█████╗  ██████╔╝"
+echo -e $lb "██╔══╝  ╚══╝██╔══██╗██╔══██║██║╚████║██║╚████║██╔══╝  ██╔══██╗"
+echo -e $lb "███████╗    ██████╦╝██║  ██║██║ ╚███║██║ ╚███║███████╗██║  ██║"
+echo -e $lb "╚══════╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚══╝╚═╝  ╚══╝╚══════╝╚═╝  ╚═╝"
+echo -e $r  "                                  Tool by - Razor Kenway"
+echo
+echo -e $y "What is Your Banner name ? : \c"
+read Bname
+echo 
+echo -e $y "What is Your name ? : \c"
+read name
+
+echo "cowsay -f eyes "$name" | lolcat " > name.txt
+echo "figlet "$Bname" | lolcat" > Bname.txt
+echo "clear" > clear.txt
+echo "PS1='\$ '" > Temp.txt
+
+
+#remove old files 
+rm -rf /data/data/com.termux/files/usr/etc/zshrc
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+
+# inject files to zshrc
+cat "clear.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "name.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "Bname.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+
+# inject files to bash.bashrc
+cat "clear.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "name.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "Bname.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+rm -rf name.txt Baname.txt clear.txt Temp.txt
+echo
+figlet Finish | lolcat
+echo -e $r "Please Restart Your Termux"
